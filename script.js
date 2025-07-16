55d0d08ba9412e29fd6573df2be359806af6bbbb
Samapadajoshi <sampadaj630@gmail.com>
2025-07-16 22:32:46 +0530

    API key added

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const apiKey = "5d57d62e4175c4884dae0bdcd78ca00b";
 const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=dhanki";
 
 async function checkWeather() {
-    const response = await fetch(apiUrl + '&appid=${apiKey}');
+    const response = await fetch(apiUrl + `&appid=${apiKey}`);
     var data = await response.json();
 
     console.log(data);
