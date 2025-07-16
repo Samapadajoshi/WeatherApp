8894619af3437860434aa4b14002edb1f6b5c5a4
Samapadajoshi <sampadaj630@gmail.com>
2025-07-16 22:48:59 +0530

    Changes for frontend done

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 const apiKey = "5d57d62e4175c4884dae0bdcd78ca00b";
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=dhanki";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=germany";
 
 async function checkWeather() {
     const response = await fetch(apiUrl + `&appid=${apiKey}`);
     var data = await response.json();
 
     console.log(data);
+    document.querySelector(".city").innerHTML = data.name;
+    //we have temp in the main son we are adding main.temp and same for humidity
+    document.querySelector(".temp").innerHTML = data.main.temp;
+    document.querySelector(".humidity").innerHTML = data.main.humidity;
+    document.querySelector(".wind").innerHTML = data.wind.speed;
 
 }
 
